961717bae711d3d06ee244176c989b47d7b073bf
Sagun Shrestha <goodsamaritan.4846@gmail.com>
2021-12-30 18:42:10 -0700

    create a base template

--- main_app/views.py
+++ main_app/views.py
@@ -5,8 +5,8 @@ from django.http import HttpResponse
 
 # Define the home view
 def home(request):
-  return HttpResponse('<h1>List your car for rent</h1>')
+  return render(request, 'base.html')
 
 
 def about(request):
-  return HttpResponse('<h3> This is the response</h3>')
+  return render(request, 'about.html')
