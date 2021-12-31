80764fde2115a443ace28cf6631e9a8957da1377
Sagun Shrestha <goodsamaritan.4846@gmail.com>
2021-12-30 18:11:04 -0700

    create html template

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+# Define the home view
+def home(request):
+  return HttpResponse('<h1>List your car for rent</h1>')
+
+
+def about(request):
+  return HttpResponse('<h3> This is the response</h3>')
