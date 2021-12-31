13f89f8be2c638e523415c769228dde32029fe8f
Sagun Shrestha <goodsamaritan.4846@gmail.com>
2021-12-31 07:29:55 -0700

    Create an intial database for cars and display an index view of all cars

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name='home'),
-  path('about/',views.about, name='about')
+  path('about/',views.about, name='about'),
+  path('cars/',views.cars_index,name='cars_index')
 
 ]
 
--- main_app/views.py
+++ main_app/views.py
@@ -10,3 +10,22 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+# Add the Car class & list and view function below the imports
+class Car:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, model, brand, mileage):
+    self.name = name
+    self.model = model
+    self.brand = brand
+    self.mileage = mileage
+
+cars = [
+  Car('Lolo', 'Accord', 'Honda', 3000),
+  Car('Sachi', 'Camry', 'Toyota', 1500),
+  Car('Fancy', 'G37', 'Infinity', 400),
+  Car('Bonk', 'M2', 'Bmw', 600),
+]
+
+
+def cars_index(request):
+  return render(request,'cars/index.html',{'cars':cars})
