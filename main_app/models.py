cb3a277ca1b287f33b3cd69c7985f7511d4616ea
Sagun Shrestha <goodsamaritan.4846@gmail.com>
2022-01-01 12:33:36 -0800

    Hook database to django with psql

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Car (models.Model):
+  name = models.CharField(max_length=100)
+  model = models.CharField(max_length=100)
+  brand = models.TextField(max_length=250)
+  mileage = models.IntegerField(blank = True)
+
+  def __str__(self):
+    return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,40 +1,18 @@
 from django.shortcuts import render
+from .models import Car
 
 # Create your views here.
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # Define the home view
 def home(request):
-  return render(request, 'base.html')
+  return render(request, 'home.html')
 
 
 def about(request):
   return render(request, 'about.html')
 
-# Add the Car class & list and view function below the imports
-class Car:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, model, brand, mileage):
-    self.name = name
-    self.model = model
-    self.brand = brand
-    self.mileage = mileage
-
-cars = [
-  Car('Sagun', 'Accord', 'Honda', 3000),
-  Car('Dipa', 'Camry', 'Toyota', 1500),
-  Car('Ben', 'G37', 'Infinity', 400),
-  Car('Jurgen', 'Leaf', 'Nissan', 600),
-  Car('Erik', 'Fx', 'Tesla', 5500),
-  Car('Elon', 'M2', 'Bmw', 6500),
-  Car('Patricia', 'M3', 'Bmw', 6500),
-  Car('Rajesh', 'Escape', 'Ford', 3300),
-  Car('Durg', 'Tahoe', 'Chevy', 7700),
-  Car('Fancy', 'Miata', 'Mazda', 400),
-  Car('Jordan', 'xod', 'Hummer', 6700),
-  Car('Jira', 'fast', 'Ferrari', 11100),
-  Car('Som', 'Veneno', 'Lamborgini', 2200),
-]
-
 
 def cars_index(request):
+  cars = Car.objects.all()
   return render(request,'cars/index.html',{'cars':cars})
