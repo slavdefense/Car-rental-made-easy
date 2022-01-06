f2554ae765e06e1c767998e2bdc13cf79a62dc2e
Sagun Shrestha <goodsamaritan.4846@gmail.com>
2022-01-06 14:40:23 -0800

    heroku

--- listorrentcars/settings.py
+++ listorrentcars/settings.py
@@ -128,4 +128,4 @@ LOGOUT_REDIRECT_URL = 'home'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
